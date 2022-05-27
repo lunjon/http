768155baea3e898a8e6c9c630eea2ed947c2b960
Jonathan <jon.lundholm@gmail.com>
2022-05-27 20:50:26 +0200

    test: add util.FileExists test

---
+++ internal/util/fs_test.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFileExists(t *testing.T) {
+	tests := []struct {
+		path   string
+		exists bool
+		isdir  bool
+	}{
+		{"fs.go", true, false},
+		{"testdata", true, true},
+		{"unknown-file", false, false},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			exists, isdir, err := FileExists(test.path)
+			assert.NoError(t, err)
+			assert.Equal(t, test.exists, exists)
+			assert.Equal(t, test.isdir, isdir)
+		})
+	}
+
+}
