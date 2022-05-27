c3bde084db347310a55666e48f54cc05c0e9f9bc
Jonathan <jon.lundholm@gmail.com>
2022-05-27 19:19:17 +0200

    feat(server): display request body

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,10 +2,21 @@ package server
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/lunjon/http/internal/format"
+)
+
+var (
+	bold      = lipgloss.NewStyle().Bold(true)
+	redB      = bold.Copy().Foreground(lipgloss.Color("1")).Render
+	greenB    = bold.Copy().Foreground(lipgloss.Color("2")).Render
+	greenish  = bold.Copy().Foreground(lipgloss.Color("48"))
+	greenishB = greenish.Copy().Bold(true)
+	grey      = bold.Copy().Foreground(lipgloss.Color("245")).Render
 )
 
 type Server struct {
@@ -15,7 +26,7 @@ type Server struct {
 
 func New(port uint, styler *format.Styler) *Server {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", createHandler(styler))
+	mux.HandleFunc("/", handler)
 
 	s := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
@@ -34,18 +45,28 @@ func (s *Server) Serve() error {
 	return s.server.ListenAndServe()
 }
 
-func createHandler(styler *format.Styler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Incoming request:")
-		fmt.Printf("  Method:  %s\n", styler.GreenB(r.Method))
-		fmt.Printf("  Path:    %s\n", styler.GreenB(r.URL.Path))
-		if len(r.Header) > 0 {
-			fmt.Println("  Headers:")
-			for name, values := range r.Header {
-				v := strings.Join(values, styler.Cyan("; "))
-				v = fmt.Sprintf("%s %s %s", styler.CyanB("["), v, styler.CyanB("]"))
-				fmt.Printf("    %s: %s\n", styler.WhiteB(name), v)
-			}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Incoming request:")
+	fmt.Printf("  Method:  %s\n", greenB(r.Method))
+	fmt.Printf("  Path:    %s\n", greenB(r.URL.Path))
+
+	if len(r.Header) > 0 {
+		fmt.Println("  Headers:")
+		for name, values := range r.Header {
+			v := strings.Join(values, grey("; "))
+			v = fmt.Sprintf("%s %s %s", greenishB.Render("["), v, greenishB.Render("]"))
+			fmt.Printf("    %s: %s\n", bold.Render(name), v)
 		}
+	}
+
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("  Body:    %s: %s\n", redB("error"), err)
+	} else if len(b) > 0 {
+		body := string(b)
+		if len(body) > 100 {
+			body = body[:90] + "..."
+		}
+		fmt.Printf("  Body:    %s\n", body)
 	}
 }
